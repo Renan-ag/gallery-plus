68df21c53467151a1c7c8ed98298b2203f961c76
Renan Andrade de Gouveia <ag.renan@hotmail.com>
2025-10-23 09:50:18 -0300

    feat: add photo and album dialog components, enhance photo details page with navigation and selectable albums

---
+++ src/contexts/albums/components/album-new-dialog.tsx
@@ -0,0 +1,113 @@
+import Button from "../../../components/button";
+import {
+  Dialog,
+  DialogBody,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "../../../components/dialog";
+import SelectCheckBoxIllustration from "../../../assets/images/select-checkbox.svg?react";
+import InputText from "../../../components/input-text";
+import Text from "../../../components/text";
+import type { Photo } from "../../photos/models/photo";
+import Skeleton from "../../../components/skeleton";
+import PhotoImageSelectable from "../../photos/components/photo-image-selectable";
+
+interface AlbumNewDialogProps {
+  trigger: React.ReactNode;
+}
+
+export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
+  const isLoadingPhotos = false;
+  const photos: Photo[] = [
+    {
+      id: "123",
+      title: "Olá Mundo",
+      imageId: "portrait-tower.png",
+      albums: [
+        { id: "123", title: "Album 1" },
+        { id: "323", title: "Album 2" },
+        { id: "513", title: "Album 3" },
+      ],
+    },
+    {
+      id: "124123",
+      title: "Olá Mundo",
+      imageId: "portrait-tower.png",
+      albums: [
+        { id: "123", title: "Album 1" },
+        { id: "323", title: "Album 2" },
+        { id: "513", title: "Album 3" },
+      ],
+    },
+  ];
+
+  function handleTogglePhoto(selected: boolean, photoId: string) {
+    console.log(selected, photoId);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>Criar Álbum</DialogHeader>
+
+        <DialogBody className="flex flex-col gap-5">
+          <InputText placeholder="Adicione um título" />
+
+          <div className="space-y-3">
+            <Text as="div" variant="label-small">
+              Fotos cadastradas
+            </Text>
+
+            {!isLoadingPhotos && photos.length > 0 && (
+              <div className="flex flex-wrap gap-2">
+                {photos.map((photo) => (
+                  <PhotoImageSelectable
+                    key={photo.id}
+                    src={`/images/${photo.imageId}`}
+                    title={photo.title}
+                    imageClassName="w-24 h-24"
+                    onSelectImage={(selected) =>
+                      handleTogglePhoto(selected, photo.id)
+                    }
+                  />
+                ))}
+              </div>
+            )}
+
+            {isLoadingPhotos && (
+              <div className="flex flex-wrap gap-2">
+                {Array.from({ length: 4 }).map((_, index) => (
+                  <Skeleton
+                    key={`photo-loading-${index}`}
+                    className="w-20 h-20 rounded-lg"
+                  />
+                ))}
+              </div>
+            )}
+
+            {!isLoadingPhotos && photos.length === 0 && (
+              <div className="w-full flex flex-col justify-center items-center gap-3">
+                <SelectCheckBoxIllustration />
+                <Text variant="paragraph-medium" className="text-center">
+                  Nenhuma foto disponível para seleção
+                </Text>
+              </div>
+            )}
+          </div>
+        </DialogBody>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="secondary">Cancelar</Button>
+          </DialogClose>
+
+          <Button>Criar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,14 +1,75 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import Container from "../components/container";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
+import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
+  const isLoadingPhoto = false;
+  const photo = {
+    id: "123",
+    title: "Olá Mundo",
+    imageId: "portrait-tower.png",
+    albums: [
+      { id: "123", title: "Album 1" },
+      { id: "323", title: "Album 2" },
+      { id: "513", title: "Album 3" },
+    ],
+  } as Photo;
 
   return (
-    <>
-      <Text>Details photo</Text>
-      <hr />
-      <Text>Photo ID: {id}</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text as="h2" variant="heading-large">
+            {photo.title}
+          </Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator loading={isLoadingPhoto} />
+      </header>
+
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo?.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="h-10 w-20" />
+          )}
+        </div>
+
+        <div className="py-3">
+          <Text as="h3" variant="heading-medium" className="mb-7">
+            Albuns
+          </Text>
+          <AlbumsListSelectable
+            loading={isLoadingPhoto}
+            photo={photo}
+            albums={[
+              { id: "123", title: "Album 1" },
+              { id: "323", title: "Album 2" },
+              { id: "513", title: "Album 3" },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
   );
 }
